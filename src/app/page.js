3c85a28945df6038ec924029a52aaceb19b1f2b4
dallinmoak <dallinmoak@gmail.com>
2023-03-24 19:55:51 -0600

    remove nav

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Nav from "@/components/nav/nav";
 import ProgramList from "@/components/program-list/program-list";
 
 export default function Home() {
   return (
    <div>
-    <Nav />
     <ProgramList />
    </div>
   )
--- src/components/nav/nav.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Nav(){
-  return(
-    <div>
-      <Link href="/admin">Admin Page</Link>
-    </div>
-  )
-}
