49ada38db9d9272f15cf473cfc67fd053bf023fa
dallinmoak <dallinmoak@gmail.com>
2023-05-06 15:57:00 -0600

    add more detailed validation messagers

--- src/common/program-validate.js
+++ src/common/program-validate.js
@@ -1,10 +1,13 @@
 export default function isValid(program) {
   let errorMsg = "";
-  if (!program?.date) errorMsg = "Bad date";
+  console.log(program.openingHymn);
+  if (!program?.date) errorMsg = "no program date provided.";
   else if (!program.presiding) errorMsg = "bad Presiding";
   else if(!program.conducting) errorMsg = "bad Conducting";
-  else if (program.openingHymn < 1) errorMsg = "bad Opening Hymn";
+  else if  (!program.openingHymn) errorMsg = "no Opening Hymn";
+  else if (program.openingHymn < 1)  errorMsg = "bad Opening Hymn";
   else if (!program.openingPrayer) errorMsg = "bad Opening Prayer";
+  else if  (!program.sacramentHymn) errorMsg = "no Sacrament Hymn";
   else if (program.sacramentHymn < 1) errorMsg = "bad Sacrament Hymn";
   else if (program.customItems){
     let hasError = false;
@@ -14,6 +17,7 @@ export default function isValid(program) {
     })
     if(hasError) errorMsg = "bad custom items";
   }
+  else if (!program.closingHymn) errorMsg = "no closing Hymn";
   else if (program.closingHymn < 1) errorMsg = "bad Closing Hymn";
   else if (!program.closingPrayer) errorMsg = "bad Closing Prayer";
   
